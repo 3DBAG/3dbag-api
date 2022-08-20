a2c981127682ae9d7cf5b615a942cbe6b94a8fd4
Balázs Dukai <balazs.dukai@gmail.com>
2022-08-20 06:29:01 +0200

    Add CRS by Requirement conformance

--- app/views.py
+++ app/views.py
@@ -150,6 +150,7 @@ def conformance():
         "conformsTo": [
             "http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/core",
             "http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/oas30",
+            "http://www.opengis.net/spec/ogcapi-features-2/1.0/conf/crs",
             "https://www.cityjson.org/specs/1.1.1"
         ]
     }
