dcc0fcf9b228854742d0111ccfadbcc809254eec
Gina Stavropoulou <gina@Ginas-MacBook-Pro.local>
2023-03-07 14:11:12 +0100

    start.py for running locally - formatted

--- start.py
+++ start.py
@@ -1,12 +1,15 @@
-from app import app, db_users, views, logging
-from sqlalchemy import exc 
+from sqlalchemy import exc
+
+from app import app, db_users, logging, views
 
 with app.app_context():
     db_users.create_all()
     logging.info("DB created")
     # db_users.drop_all()
-    b = views.UserAuth(username="user", password="1234", role=views.Permission.ADMINISTRATOR)
-    try :
+    b = views.UserAuth(
+        username="user", password="1234", role=views.Permission.ADMINISTRATOR
+    )
+    try:
         db_users.session.add(b)
         db_users.session.commit()
         logging.info("Admin created")
