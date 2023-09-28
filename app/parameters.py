5b2d1ae9ac1bfd5508bcf6a7316676c3600668fe
Gina Stavropoulou <ginastavropoulou@gmail.com>
2023-09-28 14:25:02 +0200

    error msgs

--- app/parameters.py
+++ app/parameters.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from typing import Optional, Tuple, Union
-from flask import Request, abort
+from flask import abort
 import logging
 
 DEFAULT_CRS = "http://www.opengis.net/def/crs/OGC/0/CRS84h"
@@ -27,15 +27,17 @@ class Parameters:
         try:
             self.limit = int(self.limit)
         except ValueError as error:
-            error_msg = f"Invalid parameter value. Limit must be integer.{error}"
-            logging.error(error_msg)
+            logging.error(
+                "Invalid parameter value. Limit must be integer. %s",
+                error)
             abort(400)
 
         try:
             self.offset = int(self.offset)
         except ValueError as error:
-            error_msg = f"Invalid parameter value. Offset must be integer. {error}"
-            logging.error(error_msg)
+            logging.error(
+                "Invalid parameter value. Offset must be integer. %s",
+                error)
             abort(400)
 
         if self.crs.lower() == STORAGE_CRS.lower():
@@ -66,20 +68,17 @@ class Parameters:
 
         try:
             if self.limit < 0:
-                error_msg = "Limit must be an positive integer."
-                logging.error(error_msg)
+                logging.error("Limit must be an positive integer.")
                 abort(400)
 
             if self.bbox is not None:
                 r = self.bbox.strip().strip("[]").split(',')
                 if len(r) != 4:
-                    error_msg = "BBox needs 4 parameters."
-                    logging.error(error_msg)
+                    logging.error("BBox needs 4 parameters.")
                     abort(400)
                 self.bbox = tuple(list(map(float, r)))
             else:
                 self.bbox = tuple(DEFAULT_BBOX)
         except ValueError as error:
-            error_msg = "Invalid parameter value: %s ", error
-            logging.error(error_msg)
+            logging.error("Invalid parameter value: %s ", error)
             abort(400)
