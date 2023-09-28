71989f4501c74fe773a28c914235029a1970f61d
Gina Stavropoulou <ginastavropoulou@gmail.com>
2023-09-28 13:32:22 +0200

    port mapping docker

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 server="3dbag-api.test"
+docker container stop ${server} || true
 docker build -t ${server} .
 docker run \
+  --platform linux/amd64\
   --rm \
   -p 56733:80 \
+  -p 5433:5433 \
   --name=${server} \
   -e APP_CONFIG="/app/3dbag_api_settings.cfg" \
   -v ${HOME}/data/3DBAGplus:/data/3DBAGplus \
   -v "$(pwd)":/app ${server} 
+  
