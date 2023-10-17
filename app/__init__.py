e3288b6153fad1f44f97db0cc9c9ea372c70dbfb
Gina Stavropoulou <ginastavropoulou@gmail.com>
2023-10-17 11:39:37 +0200

    test proxy

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,17 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPBasicAuth
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["POSTGRES_URL"]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 auth = HTTPBasicAuth()
 db_users = SQLAlchemy(app)
